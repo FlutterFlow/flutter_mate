ae8a34d9215d1c338c88a50e2df5c13403e57c4a
wenkaifan0720 <wenkai@flutterflow.io>
2026-01-28 17:35:46 +0800

    add script to download generated js (for hologram integration)

---
+++ apps/demo_app/lib/catalog.hologram.dart
@@ -0,0 +1,7 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_import
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
+import "package:flutter_mate/flutter_mate.dart";
